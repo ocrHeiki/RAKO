295ab996dfece27eb214dd41b6bc64f1179cf0ef
ocrHeiki <ocrheiki@gmail.com>
2025-09-18 15:30:52 +0300

    Enhance loops.py with additional print functionality and random number generation

--- kood/python/loops.py
+++ kood/python/loops.py
@@ -1,4 +1,19 @@
+import random
+
+
 names = ['Mari', 'Jüri', 'Kati', 'Juhan']
 
 # Väljasta listis olevad nimed nime kaupa eraldi real.
 for name in names:
+    print(name)
+
+print() # Tühi rida
+# Teistmoodi väljastus
+for x in range(len(names)): # x = 0..3
+    print(names[x], random.randint(1, 122))
+print()
+# Lihsad numbrid    
+for x in range(1, 5): # 1, 2, 3, 4
+    print(x, end=' ')
+print('\n') # Kaks tühja rida!
+
