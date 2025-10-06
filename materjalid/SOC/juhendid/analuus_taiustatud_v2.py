dbf994dd86f9bde015967035ae5fab95e9bd346c
Heiki Rebane <156500254+ocrHeiki@users.noreply.github.com>
2025-10-06 13:05:51 +0300

    V2

---
+++ materjalid/SOC/juhendid/analuus_taiustatud_v2.py
@@ -0,0 +1,5 @@
+# analuus_taiustatud_v2.py – täiustatud SOC logianalüüsi skript
+# (sisaldab Threat/Content Name korduste analüüsi ja TXT raporti genereerimist)
+# Autor: ocrHeiki praktikaprojekt
+
+print('Skript näidisfail loodud – tegelik analüüsikood lisatakse töömasinas.')
