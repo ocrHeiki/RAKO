62dd2d75b18015681759975586f3f2324f0b0b62
ocrHeiki <ocrheiki@gmail.com>
2025-10-09 11:57:06 +0300

    game_over

--- kood/powershell/kooliHARJUTUSED/game.ps1
+++ kood/powershell/kooliHARJUTUSED/game.ps1
@@ -23,5 +23,28 @@ function Show-Scoreboard {
         Write-Host $line.split(";")
     }
 }
-Show-Scoreboard
+
+function LetsPlay {
+    [int]$user_nr = Read-Host "Sisesta number"
+    $Global:steps++ #Global:steps += 1
+    $game_over = $false
+
+    if(($user_nr -gt $pc_nr) -and ($user_nr -ne 1000)) {
+      Write-Host "Väiksem"
+
+    } elseif (($user_nr -lt $pc_nr) -and ($user_nr -ne 1000)) {
+    } elseif (($user_nr -eq $pc_nr) -and ($user_nr -ne 1000)) {
+        Write-Host "Leidsid õige numbri $Global:steps sammuga."
+        $game_over = $true
+    } elseif (($user_nr -eq 1000)) {
+        Write-Host "Leidsid mu nõrga koha. Õige number on $pc_nr"
+    }   
+        return $game_over #Tagastab tulemuse $true või $false
+}
+while($game_over -eq $false) {
+    $game_over =  LetsPlay
+}
+
+
+#Show-Scoreboard
 #writeToFile
