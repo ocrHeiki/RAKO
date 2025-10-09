e28d36cf2b3f236c7244b4939af368233c973dac
ocrHeiki <ocrheiki@gmail.com>
2025-10-09 11:16:07 +0300

    up

---
+++ kood/powershell/kooliHARJUTUSED/game.ps1
@@ -0,0 +1,13 @@
+<#
+Äraarvamise mäng. Numbrid 1-100
+Tagauks
+Kui mäng on läbi, siis küsitakse nime ja koos käikudega/sammudega salvestatakse see faili
+#>
+$pc_nr = Get-Random -Minimum 1 -Maximum 100
+[System.Boolean]$game_over = $false
+$Global:steps = 0   #Globaalne muutuja sammud
+$filename = Join-Path -Path $PSScriptRoot -ChildPath "result.txt"
+Clear-Host
+Write-Host $pc_nr $filename #Kontrolliks
+Clear-Host
+
