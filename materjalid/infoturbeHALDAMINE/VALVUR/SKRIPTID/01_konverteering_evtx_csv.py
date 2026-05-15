ec1a48415b338b00c1c3e93356e9aa900bdcd42f
Heiki Rebane <156500254+ocrHeiki@users.noreply.github.com>
2026-05-15 12:02:52 +0300

    Improve header comment in 01_konverteering_evtx_csv.py
    Updated the script header comment for clarity.

--- materjalid/infoturbeHALDAMINE/VALVUR/SKRIPTID/01_konverteering_evtx_csv.py
+++ materjalid/infoturbeHALDAMINE/VALVUR/SKRIPTID/01_konverteering_evtx_csv.py
@@ -23,7 +23,7 @@
 #                                                                             #
 ###############################################################################
 """
-
+"""
 01_konverteering_evtx_csv.py - Windowsi .evtx logide konverteerimine CSV-formaati.
 Kasutamine: python3 SKRIPTID/01_konverteering_evtx_csv.py --path LOGID
 """
